5c388865b9390c0192b70d8dfea5c6416eafc3d8
Riyaz J <j.riyazu@gmail.com>
2021-05-18 12:37:58 +0530

    Create Home Page Component.

---
+++ src/app/components/home-page/home-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class HomePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
