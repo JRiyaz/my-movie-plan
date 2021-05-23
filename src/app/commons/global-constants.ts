cbced106edd5d0a8177490152b82beec4d5569b1
Riyaz J <j.riyazu@gmail.com>
2021-05-23 12:35:35 +0530

    Update GlobalConstants class.

--- src/app/commons/global-constants.ts
+++ src/app/commons/global-constants.ts
@@ -1,4 +1,66 @@
 export class GlobalConstants {
 
     public static APP_NAME: string = 'NMS Cinemas';
+
+    public static ROOT_URL = 'http://localhost:5555';
+
+    public static REGISTER_URL = `${GlobalConstants.ROOT_URL}/user/sign-up`;
+
+    public static CHECK_UNIQUENESS_URL = `${GlobalConstants.ROOT_URL}/user/check`;
+
+    public static AUTHENTICATE_URL = `${GlobalConstants.ROOT_URL}/user/authenticate`;
+
+    public static UNIQUE_USER_URL = `${GlobalConstants.ROOT_URL}/user/check`;
+
+    public static FORGOT_PASSWORD_URL = `${GlobalConstants.ROOT_URL}/user/forgot-password`;
+
+    public static AUDITORIUM_URL = `${GlobalConstants.ROOT_URL}/auditorium`;
+
+    public static GET_AUDITORIUM_URL = `${GlobalConstants.ROOT_URL}/auditorium/add`;
+
+    public static ADD_AUDITORIUM_URL = `${GlobalConstants.ROOT_URL}/auditorium/add`;
+
+    public static UPDATE_AUDITORIUM_URL = `${GlobalConstants.ROOT_URL}/auditorium/add`;
+
+    public static DELETE_AUDITORIUM_URL = `${GlobalConstants.ROOT_URL}/auditorium/add`;
+
+    public static MOVIE_URL = `${GlobalConstants.ROOT_URL}/movie`;
+
+    public static GET_MOVIE_URL = `${GlobalConstants.ROOT_URL}/auditorium/add`;
+
+    public static ADD_MOVIE_URL = `${GlobalConstants.ROOT_URL}/auditorium/add`;
+
+    public static UPDATE_MOVIE_URL = `${GlobalConstants.ROOT_URL}/auditorium/add`;
+
+    public static DELETE_MOVIE_URL = `${GlobalConstants.ROOT_URL}/auditorium/add`;
+
+    public static SHOW_URL = `${GlobalConstants.ROOT_URL}/show`;
+
+    public static GET_SHOW_URL = `${GlobalConstants.ROOT_URL}/auditorium/add`;
+
+    public static ADD_SHOW_URL = `${GlobalConstants.ROOT_URL}/auditorium/add`;
+
+    public static UPDATE_SHOW_URL = `${GlobalConstants.ROOT_URL}/auditorium/add`;
+
+    public static DELETE_SHOW_URL = `${GlobalConstants.ROOT_URL}/auditorium/add`;
+
+    public static MOVIESHOWS_URL = `${GlobalConstants.ROOT_URL}/movie`;
+
+    public static GET_MOVIESHOWS_URL = `${GlobalConstants.ROOT_URL}/auditorium/add`;
+
+    public static ADD_MOVIESHOWS_URL = `${GlobalConstants.ROOT_URL}/auditorium/add`;
+
+    public static UPDATE_MOVIESHOWS_URL = `${GlobalConstants.ROOT_URL}/auditorium/add`;
+
+    public static DELETE_MOVIESHOWS_URL = `${GlobalConstants.ROOT_URL}/auditorium/add`;
+
+    public static BOOKING_URL = `${GlobalConstants.ROOT_URL}/booking`;
+
+    public static GET_BOOKING_URL = `${GlobalConstants.ROOT_URL}/auditorium/add`;
+
+    public static ADD_BOOKING_URL = `${GlobalConstants.ROOT_URL}/auditorium/add`;
+
+    public static UPDATE_BOOKING_URL = `${GlobalConstants.ROOT_URL}/auditorium/add`;
+
+    public static DELETE_BOOKING_URL = `${GlobalConstants.ROOT_URL}/auditorium/add`;
 }
